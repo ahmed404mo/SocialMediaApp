f7c9726784104d5dc6baf2bd1d1248d4aec4293f
ahmed mokhtar <mo879938@gmail.com>
2026-05-29 01:27:04 +0300

    add vercel.config ++11

--- src/common/services/notification.service.ts
+++ src/common/services/notification.service.ts
@@ -1,50 +1,66 @@
-import admin from "firebase-admin"
+import admin from "firebase-admin";
 import { readFileSync } from "node:fs";
 import { resolve } from "node:path";
 
-export class NotificationService{
-  private client:admin.app.App
-  constructor(){
-    
-var serviceAccount = JSON.parse(readFileSync(resolve('./src/config/notifaction-4a435-firebase-adminsdk-fbsvc-1ecfd772af.json'))as unknown as string)
-this.client = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-}
-async sendNotification({
-  token, 
-  data
-    
-  }:{
-    token:string, 
-    data:{
-      title:string,
-      body:string
+export class NotificationService {
+  private client: admin.app.App | undefined;
+  constructor() {
+    try {
+      let serviceAccount;
+      if (process.env.FIREBASE_CREDENTIALS) {
+        serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+      } else {
+        serviceAccount = JSON.parse(
+          readFileSync(
+            resolve(
+              "./src/config/notifaction-4a435-firebase-adminsdk-fbsvc-1ecfd772af.json",
+            ),
+            "utf8",
+          ),
+        );
+      }
+      this.client = admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+      });
+    } catch (error) {
+      console.warn(
+        "⚠️ Firebase Admin initialization skipped: Credentials not found or invalid.",
+      );
     }
-  }){
-const message= {
-  token,
-  data
-}
-return await this.client.messaging().send(message)
-}
-async sendNotifications({
-  tokens, 
-  data
-    
-  }:{
-    tokens:string[], 
-    data:{
-      title:string,
-      body:string
-    }
-  }){
-await Promise.allSettled(
-  tokens.map(token=>{
-    return this.sendNotification({token, data})
-  })
-)
-}
+  }
+  async sendNotification({
+    token,
+    data,
+  }: {
+    token: string;
+    data: {
+      title: string;
+      body: string;
+    };
+  }) {
+    if (!this.client) return;
+    const message = {
+      token,
+      data,
+    };
+    return await this.client.messaging().send(message);
+  }
+  async sendNotifications({
+    tokens,
+    data,
+  }: {
+    tokens: string[];
+    data: {
+      title: string;
+      body: string;
+    };
+  }) {
+    await Promise.allSettled(
+      tokens.map((token) => {
+        return this.sendNotification({ token, data });
+      }),
+    );
+  }
 }
 
-export const notificationService= new NotificationService()
+export const notificationService = new NotificationService();
